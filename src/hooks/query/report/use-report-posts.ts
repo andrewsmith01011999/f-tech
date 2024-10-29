ed82e0842a7dde361421d243d7bfac4ef924a2e8
khoai <khoaidaica@gmail.com>
2024-10-29 21:52:39 +0700

    fix: filter report post

--- src/hooks/query/report/use-report-posts.ts
+++ src/hooks/query/report/use-report-posts.ts
@@ -1,6 +1,6 @@
 import { request } from '@/apis/request';
 import { feedbackKeys } from '@/consts/factory/feedback';
-import { CreateFeedbackPayload, Feedback } from '@/types/feedback/feedback';
+import { CreateFeedbackPayload, Feedback, FeedbackStatus } from '@/types/feedback/feedback';
 import { PaginationParams } from '@/types';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { AccountReport, PostReport } from '@/types/report/report';
@@ -8,13 +8,43 @@ import { reportKeys } from '@/consts/factory/report';
 
 export type FeedbackListingParams = PaginationParams;
 
-type FeedbackListingProps = {};
+export type PostReportParams = PaginationParams & {
+    reportPostStatusList?: FeedbackStatus[];
+};
 
-export const useReportPostsListing = () => {
+type PostReportProps = {
+    params: PostReportParams;
+};
+
+export const useReportPostsListing = ({ params }: PostReportProps) => {
     const fetchPostReport = async () => {
         const { entity } = await request<PostReport[]>(
             'get',
-            '/post-report/getall',
+            '/post-report/filter',
+            {},
+            {
+                params,
+                paramsSerializer: {
+                    indexes: null,
+                },
+            },
+        );
+
+        return entity;
+    };
+
+    return useQuery<PostReport[]>({
+        queryKey: reportKeys.reportPostListing(params),
+        queryFn: fetchPostReport,
+        placeholderData: keepPreviousData,
+    });
+};
+
+export const useReportAccountListing = () => {
+    const fetchAccountReport = async () => {
+        const { entity } = await request<AccountReport[]>(
+            'get',
+            '/report-account/get-all',
             {},
             {
                 paramsSerializer: {
@@ -26,32 +56,9 @@ export const useReportPostsListing = () => {
         return entity;
     };
 
-    return useQuery<PostReport[]>({
-        queryKey: reportKeys.reportPostListing(),
-        queryFn: fetchPostReport,
+    return useQuery<AccountReport[]>({
+        queryKey: reportKeys.reportAccountListing(),
+        queryFn: fetchAccountReport,
         placeholderData: keepPreviousData,
     });
 };
-
-export const useReportAccountListing = () => {
-      const fetchAccountReport = async () => {
-          const { entity } = await request<AccountReport[]>(
-              'get',
-              '/report-account/get-all',
-              {},
-              {
-                  paramsSerializer: {
-                      indexes: null,
-                  },
-              },
-          );
-
-          return entity;
-      };
-
-      return useQuery<AccountReport[]>({
-          queryKey: reportKeys.reportAccountListing(),
-          queryFn: fetchAccountReport,
-          placeholderData: keepPreviousData,
-      });
-}
--- src/pages/admin/report/components/admin-report-list.tsx
+++ src/pages/admin/report/components/admin-report-list.tsx
@@ -1,20 +1,127 @@
 import { useFeedbackListing } from '@/hooks/query/feedback/use-feedback-listing';
-import { Empty } from 'antd';
+import { Button, Checkbox, Empty, Flex, GetProp, Popover, Tag, Typography } from 'antd';
 import React from 'react';
 import AdminFeedbackWrapper from '../../feedback/layout/admin-feedback-wrapper';
 import AdminFeedbackItem from '../../feedback/components/admin-feedback-item';
-import { useReportPostsListing } from '@/hooks/query/report/use-report-posts';
+import { PostReportParams, useReportPostsListing } from '@/hooks/query/report/use-report-posts';
 import AdminReportItem from './admin-report-item';
+import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE } from '@/consts/common';
+import { FilterOutlined } from '@ant-design/icons';
+import { mapFeedbackStatusColor } from '../../feedback/utils/map-feedback-status-color';
+import { FeedbackStatus } from '@/types/feedback/feedback';
 
 const AdminReportList = () => {
-    const { data: reportPosts } = useReportPostsListing();
+    const initialParams: PostReportParams = {
+        page: DEFAULT_PAGE,
+        perPage: DEFAULT_PAGE_SIZE,
+    };
+
+    const [selectedStatus, setSelectedStatus] = React.useState<string[]>([]);
+    const [params, setParams] = React.useState<PostReportParams>(initialParams);
+
+    const { data: reportPosts } = useReportPostsListing({
+        params
+    });
 
     if (!reportPosts || !reportPosts.length) {
         return <Empty />;
     }
 
+    const optionsWithDisabled = [
+        {
+            label: (
+                <Tag
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 24,
+                        fontSize: 12,
+                    }}
+                    color={mapFeedbackStatusColor('PENDING')}
+                >
+                    REJECTED
+                </Tag>
+            ),
+            value: 'PENDING',
+        },
+        {
+            label: (
+                <Tag
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 24,
+                        fontSize: 12,
+                    }}
+                    color={mapFeedbackStatusColor('REJECTED')}
+                >
+                    REJECTED
+                </Tag>
+            ),
+            value: 'REJECTED',
+        },
+        {
+            label: (
+                <Tag
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: 24,
+                        fontSize: 12,
+                    }}
+                    color={mapFeedbackStatusColor('APPROVED')}
+                >
+                    APPROVED
+                </Tag>
+            ),
+            value: 'APPROVED',
+        },
+    ];
+
+    const onChange: GetProp<typeof Checkbox.Group, 'onChange'> = checkedValues => {
+        setSelectedStatus(checkedValues as string[]);
+    };
+
+    const content = (
+        <Flex vertical justify="center" align="center" gap={12}>
+            <Typography.Title level={5}>STATUS</Typography.Title>
+            <Checkbox.Group onChange={onChange}>
+                <Flex vertical gap={8}>
+                    {optionsWithDisabled.map(option => (
+                        <Checkbox key={option.value} value={option.value}>
+                            {option.label}
+                        </Checkbox>
+                    ))}
+                </Flex>
+            </Checkbox.Group>
+
+            <Button
+                type="primary"
+                onClick={() =>
+                    setParams({
+                        ...params,
+                        reportPostStatusList: selectedStatus as FeedbackStatus[],
+                    })
+                }
+            >
+                Apply
+            </Button>
+        </Flex>
+    );
+
     return (
         <AdminFeedbackWrapper>
+            <Flex justify="space-between">
+                <div></div>
+
+                <Popover content={content} trigger="click" arrow={false}>
+                    <Button icon={<FilterOutlined />}>Filter</Button>
+                </Popover>
+            </Flex>
+
             {reportPosts.map(reportPost => (
                 <AdminReportItem key={reportPost.reportId} data={reportPost} />
             ))}
