0e18d70fd0adbf9d82f7b90e31fd1b910174fb85
khoai <khoaidaica@gmail.com>
2024-11-04 16:30:28 +0700

    fix: change endpoint reward

--- src/hooks/query/transaction/use-transactions-current-account.ts
+++ src/hooks/query/transaction/use-transactions-current-account.ts
@@ -1,23 +1,26 @@
-import { request } from "@/apis/request";
-import { transactionKeys } from "@/consts/factory/transaction";
-import { Transaction } from "@/types/transaction/transaction";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { request } from '@/apis/request';
+import { transactionKeys } from '@/consts/factory/transaction';
+import { Transaction } from '@/types/transaction/transaction';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
 export const useTransactionsCurrentAccount = () => {
-  const fetchTransactionsCurrentAccount = async () : Promise<Transaction[]> => {
-    const {
-      entity
-    } = await request<Transaction[]>('get', '/transaction/get-transactions-of-current-account', {
-      params: {
-        indexes: null
-      }
-    });
-    return entity;
-  };
+    const fetchTransactionsCurrentAccount = async (): Promise<Transaction[]> => {
+        const { entity } = await request<Transaction[]>(
+            'get',
+            '/transaction/getall/by-current-user',
+            {},
+            {
+                paramsSerializer: {
+                    indexes: null,
+                },
+            },
+        );
+        return entity;
+    };
 
-  return useQuery<Transaction[]>({
-    queryKey: transactionKeys.currentAccount(),
-    queryFn: fetchTransactionsCurrentAccount,
-    placeholderData: keepPreviousData
-  });
-}
+    return useQuery<Transaction[]>({
+        queryKey: transactionKeys.currentAccount(),
+        queryFn: fetchTransactionsCurrentAccount,
+        placeholderData: keepPreviousData,
+    });
+};
--- src/types/transaction/transaction.ts
+++ src/types/transaction/transaction.ts
@@ -4,3 +4,21 @@ export type Transaction = {
     type: string;
     createdDate: string
 };
+
+export type DailyPointTransaction = {
+    dailyPointId: string,
+    pointEarned: number,
+    createdDate: string
+}
+
+export type BonusPoint = {
+    dailyPointId: string,
+    pointEarned: number,
+    createdDate: string
+}
+
+export type FilterTransaction = {
+    transactionList: Transaction[];
+    dailyPointList: DailyPointTransaction[];
+    bonusPoint: BonusPoint[];
+}
