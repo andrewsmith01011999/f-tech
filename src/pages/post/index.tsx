d3ea4f9a998f9d7bb72d33f9115210b76690e144
khoai <khoaidaica@gmail.com>
2024-12-03 21:19:57 +0700

    feat: add tab

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,7 +1,27 @@
+import { BaseTab } from '@/components/core/tab';
+import { TabsProps } from 'antd';
+import { ExplorePostList } from './components/explore-post-list';
 import { HomePostList } from './components/home-post-list';
 
 const PostPage = () => {
-    return <HomePostList />;
+    const items: TabsProps['items'] = [
+        {
+            key: '1',
+            label: 'Home',
+            children: <HomePostList />,
+        },
+        {
+            key: '2',
+            label: 'Explore',
+            children: <ExplorePostList />,
+        },
+    ];
+
+    return (
+        <>
+            <BaseTab items={items} defaultActiveKey="1" />
+        </>
+    );
 };
 
 export default PostPage;
