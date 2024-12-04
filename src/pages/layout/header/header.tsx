2e7bdee77c9dd2cd3fa7355d6b1d394014217949
khoai <khoaidaica@gmail.com>
2024-12-04 23:58:56 +0700

    chore: remove noti

--- src/pages/layout/header/header.tsx
+++ src/pages/layout/header/header.tsx
@@ -68,11 +68,12 @@ const HeaderComponent: FC<HeaderProps> = ({ collapsed, toggle }) => {
         if (
             notifications?.length !== undefined &&
             localStorage.getItem('count') !== undefined &&
+            localStorage.getItem('count') !== '' &&
             notifications?.length > Number(localStorage.getItem('count'))
         ) {
-            openNotification(notifications?.[0]?.title, notifications?.[0]?.message);
+            // openNotification(notifications?.[0]?.title, notifications?.[0]?.message);
             timeOut = setTimeout(() => {
-                localStorage.setItem('count', notifications?.length.toString() || '0');
+                localStorage.setItem('count', notifications?.length.toString() || '');
             }, 5000);
         } else {
             localStorage.setItem('count', notifications?.length.toString() || '0');
